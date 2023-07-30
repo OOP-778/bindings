fcafbec3152acb6bf0d20f2a9c24e19b2397d4e3
oop778 <oskardhavel@gmail.com>
2023-07-30 14:49:09 +0300

    Add Bindings prefix to properties

--- src/main/java/dev/oop778/bindings/BindingsOptions.java
+++ src/main/java/dev/oop778/bindings/BindingsOptions.java
@@ -1,7 +1,7 @@
 package dev.oop778.bindings;
 
 public class BindingsOptions {
-    public static long STACK_SIZE_LIMIT = Long.parseLong(System.getProperty("stackSizeLimit", "5"));
-    public static boolean ENABLE_TRACING = Boolean.parseBoolean(System.getProperty("enableTracing", "false"));
-    public static boolean TRACK_CLOSING = Boolean.parseBoolean(System.getProperty("trackClosing", "false"));
+    public static long STACK_SIZE_LIMIT = Long.parseLong(System.getProperty("BindingsStackSizeLimit", "5"));
+    public static boolean ENABLE_TRACING = Boolean.parseBoolean(System.getProperty("BindingsTracing", "false"));
+    public static boolean TRACK_CLOSING = Boolean.parseBoolean(System.getProperty("BindingsTrackClosing", "false"));
 }
