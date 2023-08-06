a4d0a6eeb81b82259bb587eac16d8442876ef497
oop778 <oskardhavel@gmail.com>
2023-08-06 14:34:47 +0300

    "Renamed MemoryLeakTesting to General and updated tests"
    The class MemoryLeakTesting.java was renamed to General.java to reflect its more general functionality. Besides the renaming, multiple assertions were replaced with a assertNotNull assertion and two new tests for multiple times binding and circular binding were added to enhance the robustness of the testing suite. This will provide a more comprehensive evaluation of the binding capabilities.

--- src/test/java/test/oop778/binding/test/General.java
+++ src/test/java/test/oop778/binding/test/General.java
@@ -1,13 +1,13 @@
 package test.oop778.binding.test;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import dev.oop778.bindings.Bindings;
 import dev.oop778.bindings.type.Bindable;
 import org.junit.jupiter.api.Test;
 
-class MemoryLeakTesting {
+class General {
 
     @Test
     void testChildrenClosingRemovedFromParent() {
@@ -29,5 +29,32 @@ class MemoryLeakTesting {
 
         parent.close();
         assertFalse(Bindings.getInstance().contains(parent), "Parent is still binded to child");
+    }
+
+    @Test
+    void testMultipleTimesBind() {
+        final Bindable parent = Bindable.create();
+        final Bindable child = Bindable.create();
+
+        child.bindTo(parent);
+        child.bindTo(parent);
+        child.bindTo(parent);
+    }
+
+    @Test
+    void testCircularBindings() {
+        final Bindable parent = Bindable.create();
+        final Bindable child = Bindable.create();
+
+        child.bindTo(parent);
+
+        Throwable throwable = null;
+        try {
+            parent.bindTo(child);
+        } catch (Throwable error) {
+            throwable = error;
+        }
+
+        assertNotNull(throwable, "Circular binding is not detected");
     }
 }
