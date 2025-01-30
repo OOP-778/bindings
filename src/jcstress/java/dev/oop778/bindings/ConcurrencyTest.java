6132eecb3d4e03547c9633e8ce75f553b29a0979
oop778 <oskardhavel@gmail.com>
2025-01-30 08:50:49 +0200

    Updated Concurrency close & bind with multiple layers

--- src/jcstress/java/dev/oop778/bindings/ConcurrencyTest.java
+++ src/jcstress/java/dev/oop778/bindings/ConcurrencyTest.java
@@ -1,31 +1,20 @@
-package jcstress.dev.oop778.binding;
+package dev.oop778.bindings;
 
-import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
-
-import dev.oop778.bindings.Bindings;
+import dev.oop778.bindings.enums.BindingOrder;
 import dev.oop778.bindings.type.Bindable;
+import dev.oop778.bindings.type.BindableOnce;
 import org.openjdk.jcstress.annotations.Actor;
 import org.openjdk.jcstress.annotations.JCStressTest;
 import org.openjdk.jcstress.annotations.Outcome;
 import org.openjdk.jcstress.annotations.State;
 import org.openjdk.jcstress.infra.results.ZZ_Result;
 
+import static org.openjdk.jcstress.annotations.Expect.ACCEPTABLE;
+
 @JCStressTest
 @Outcome(id = {"true, false"}, expect = ACCEPTABLE, desc = "Yes")
 @Outcome(id = {"false, true"}, expect = ACCEPTABLE, desc = "Yes")
 public class ConcurrencyTest {
-
-    @State
-    public static class TestState {
-        private final Bindable bindable1;
-        private final Bindable bindable2;
-
-        public TestState() {
-            this.bindable1 = Bindable.create();
-            this.bindable2 = Bindable.create();
-            bindable2.bindTo(bindable1);
-        }
-    }
 
     @Actor
     public void actor1(TestState state, ZZ_Result result) {
@@ -34,7 +23,18 @@ public class ConcurrencyTest {
 
     @Actor
     public void actor2(TestState state, ZZ_Result result) {
-        state.bindable2.bindTo(state.bindable1);
-        result.r2 = state.bindable1.isBinded(state.bindable2);
+        result.r2 = Bindings.getInstance().bind(state.bindable2, state.bindable1, BindingOrder.NORMAL);
+    }
+
+    @State
+    public static class TestState {
+        private final BindableOnce bindable1;
+        private final BindableOnce bindable2;
+
+        public TestState() {
+            this.bindable1 = new TestBindable();
+            this.bindable2 = new TestBindable();
+            this.bindable1.alive().bindTo(Bindable.create());
+        }
     }
 }
