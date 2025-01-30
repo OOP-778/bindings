dc5973d5790710fe92d8026d4a1b37b6423315e5
oop778 <oskardhavel@gmail.com>
2025-01-30 08:47:06 +0200

    Add bit mask to BindableFlag

--- src/main/java/dev/oop778/bindings/enums/BindableFlag.java
+++ src/main/java/dev/oop778/bindings/enums/BindableFlag.java
@@ -3,7 +3,16 @@ package dev.oop778.bindings.enums;
 public enum BindableFlag {
     // Will close bindable once all children unbind from it
     CLOSE_WHEN_EMPTY,
-
     // Close when first children unbinds
-    CLOSE_ON_UNBIND,
+    CLOSE_ON_UNBIND;
+
+    private final int mask;
+
+    BindableFlag() {
+        this.mask = 1 << this.ordinal();
+    }
+
+    public int getMask() {
+        return this.mask;
+    }
 }
