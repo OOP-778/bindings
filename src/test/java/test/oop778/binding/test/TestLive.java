8731fe2c21c72f1366dbecbbd978b25370ca9ee3
oop778 <oskardhavel@gmail.com>
2025-01-30 08:48:15 +0200

    Change test Live to use 50 threads

--- src/test/java/test/oop778/binding/test/TestLive.java
+++ src/test/java/test/oop778/binding/test/TestLive.java
@@ -1,23 +1,31 @@
 package test.oop778.binding.test;
 
 import dev.oop778.bindings.Bindings;
+import dev.oop778.bindings.BindingsDumper;
+import dev.oop778.bindings.extras.BindingsStackTracker;
+import dev.oop778.bindings.extras.BindingsTimeStamp;
 import dev.oop778.bindings.type.Bindable;
+import lombok.SneakyThrows;
+
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ThreadLocalRandom;
-import lombok.SneakyThrows;
 
 public class TestLive {
     private final List<Bindable> bindables = new CopyOnWriteArrayList<>();
     private final CountDownLatch latch;
-
     private static final int NUMBER_OF_BINDABLES = 100;
     private static final int NUMBER_OF_ITERATIONS = 20;
-    private static final int THREADS = 10;
+    private static final int THREADS = 50;
 
     private TestLive() {
+        BindingsTimeStamp.install();
+        BindingsStackTracker.install(5);
+
+        final Bindings instance = Bindings.getInstance();
+
         this.latch = new CountDownLatch(THREADS);
         for (int i = 0; i < THREADS; i++) {
             final int finalI = i;
@@ -41,6 +49,18 @@ public class TestLive {
         }
     }
 
+    public static void main(String[] args) {
+        start();
+    }
+
+    @SneakyThrows
+    public static void start() {
+        final TestLive testLive = new TestLive();
+        testLive.latch.await();
+
+        BindingsDumper.dumpToFile(Paths.get(System.getProperty("user.dir")).resolve("dump.dump"));
+    }
+
     private void tick(int thread) {
         for (int i = 0; i < NUMBER_OF_BINDABLES; i++) {
             // Add new bindable
@@ -52,7 +72,7 @@ public class TestLive {
             if (ThreadLocalRandom.current().nextBoolean()) {
                 final Bindable what = this.bindables.get(ThreadLocalRandom.current().nextInt(this.bindables.size() - 1));
                 final Bindable to = this.bindables.get(ThreadLocalRandom.current().nextInt(this.bindables.size() - 1));
-                if (to != what) {
+                if (to != what && !to.isBinded(what)) {
                     what.bindTo(to);
                 }
             }
@@ -63,17 +83,5 @@ public class TestLive {
                 what.close();
             }
         }
-    }
-
-    public static void main(String[] args) {
-        start();
-    }
-
-    @SneakyThrows
-    public static void start() {
-        final TestLive testLive = new TestLive();
-        testLive.latch.await();
-
-        Bindings.getInstance().dumpToFile(Paths.get(System.getProperty("user.dir")).resolve("dump.html").toFile());
     }
 }
